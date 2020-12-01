588d16c8b9b153e6bc92f4b2c18fdb51226ddd2a
rhayeksa <rhayeksads@gmail.com>
2020-12-01 21:04:53 +0700

    finished Home pages

---
+++ application/views/pages/home.php
@@ -0,0 +1,26 @@
+<div class="card-deck">
+  <div class="card bg-light">
+    <div class="card-header">Mahasiswa</div>
+    <div class="card-body">
+      <a href="<?= base_url(); ?>Mahasiswa" class="btn btn-primary">More</a>
+    </div>
+  </div>
+  <div class="card bg-light">
+    <div class="card-header">Dosen</div>
+    <div class="card-body">
+      <a href="<?= base_url(); ?>Dosen" class="btn btn-primary">More</a>
+    </div>
+  </div>
+  <div class="card bg-light">
+    <div class="card-header">Mata Kuliah</div>
+    <div class="card-body">
+      <a href="<?= base_url(); ?>Mata_kuliah" class="btn btn-primary">More</a>
+    </div>
+  </div>
+  <!-- <div class="card bg-light">
+    <div class="card-header">Hasil Study</div>
+    <div class="card-body">
+      <a href="<?= base_url(); ?>Hasil_study" class="btn btn-primary">More</a>
+    </div>
+  </div> -->
+</div>
