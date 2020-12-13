8baffdf94effe8df79bbfae251680667f887d2d6
rhayeksa <rhayeksads@gmail.com>
2020-12-14 02:27:14 +0700

    fixed security database

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://127.0.0.1/CodeIgniter-app-kampus/';
+$config['base_url'] = 'http://127.0.2.1/kampus-Aplication-CodeIgniter3-mysql/';
 
 /*
 |--------------------------------------------------------------------------
